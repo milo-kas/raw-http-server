fea35de52863c0b04261b940619d86119202e2db
milo-kas <49353384+milo-kas@users.noreply.github.com>
2026-06-29 20:12:40 +0200

    feat: add basic ServerSocket and input stream

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws Exception {
+        int port = 8080;
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        Socket clientSocket = serverSocket.accept();
+
+        // Read and Translate incoming raw HTTP request from the browser to text
+        BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+
+        clientSocket.close();
     }
 }
