12d9c5a22dbc96210891cf559ffe6ff101d0e187
milo-kas <49353384+milo-kas@users.noreply.github.com>
2026-07-22 20:52:52 +0200

    feat: add headers for closing TCP connection and RFC 9110 date

--- src/main/java/com/github/mksafe/http/Response.java
+++ src/main/java/com/github/mksafe/http/Response.java
@@ -3,6 +3,11 @@ package com.github.mksafe.http;
 import java.io.IOException;
 import java.io.OutputStream;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Response {
 
     private final Status status;
@@ -19,33 +24,40 @@ public class Response {
 
     // Send actual response
     public void respond(OutputStream outputStream) throws IOException {
-        // TODO: Support custom responses
-
-        // status
-        // content type
-        // CRLF
-        // payload
-
-        // Stream back a successful response (status code 200 - 299)
+        // Stream the response status line
         outputStream.write(formatHeader("HTTP/1.1 " + status.getCode() + " "+ status.getMessage()));
         // Stream content type
 //        outputStream.write(formatHeader("Content-Type: text/html"));
         outputStream.write(formatHeader("Content-Type: " + contentType));
         outputStream.write(formatHeader("Content-Length: " + payload.length));
+        // Signal the end of the TCP connection after response
+        outputStream.write(formatHeader("Connection: close"));
+        // Stream date
+        outputStream.write(formatHeader("Date: " + getDate()));
+
         // Stream empty line to signal the end of headers
         outputStream.write(formatHeader(""));
-        // Send headers
-        outputStream.flush();
 
         // Stream payload if requested
         if (method.equals(Method.GET)) {
             outputStream.write(payload);
-            outputStream.flush();
         }
+
+        // Flush buffered bytes to be written to underlying socket
+        outputStream.flush();
     }
 
     // CRLF helper
     private static byte[] formatHeader(String headerString) {
         return (headerString +  "\r\n").getBytes();
+    }
+
+    // Date helper with format as per RFC 9110
+    private static String getDate() {
+        DateTimeFormatter RFC_9110_HTTP_DATE = DateTimeFormatter
+                .ofPattern("EEE, dd MMM yyyy HH:mm:ss 'GMT'", Locale.ENGLISH).withZone(ZoneId.of("GMT"));
+
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        return RFC_9110_HTTP_DATE.format(zonedDateTime);
     }
 }
